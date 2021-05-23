6049e0d8ad745c0418f7b50045a50760313b4598
rockemma <jossueemmanuelfuentes@gmail.com>
2021-05-23 17:23:01 -0600

    se agregan las siguientes configuraciones: - libreria para efectos - js de index-effects con el efecto a la sección de contactos - optimización de código del banner

---
+++ js/index-effects.js
@@ -0,0 +1,7 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.contact-box',{
+        duration:1000, /* duración de la animación */
+        origin: 'botton', /* punto de originen de la animación */
+        distance: '-10px' /* distancia del origen de la animatión */
+    });
